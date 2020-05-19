e9776c9eb694bbec988a18685bfb8ab74efdb1f8
npollock14 <33988166+npollock14@users.noreply.github.com>
2020-05-19 16:00:31 -0700

    abstract projectile and sector classes started

--- Drone Constructor - CSQ4 Game/src/Projectile.java
+++ Drone Constructor - CSQ4 Game/src/Projectile.java
@@ -6,4 +6,14 @@ double rVel;
 double rotation;
 boolean alive = true;
 double maxLifeSpan = 1000;
+double damage;
+
+public Projectile(Poly bounds, Vec2 vel, double rVel, double rotation, double maxLifeSpan, double damage) {
+	this.bounds = bounds;
+	this.vel = vel;
+	this.rVel = rVel;
+	this.rotation = rotation;
+	this.maxLifeSpan = maxLifeSpan;
+	this.damage = damage;
+}
 }
--- Drone Constructor - CSQ4 Game/src/Sector.java
+++ Drone Constructor - CSQ4 Game/src/Sector.java
@@ -1,7 +1,14 @@
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 public abstract class Sector {
 	ArrayList<Ship> ships = new ArrayList<Ship>();
 	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	
+	public abstract void update();
+	
+	public abstract void draw(Graphics2D g);
+	
+	public abstract void init();
 
 }

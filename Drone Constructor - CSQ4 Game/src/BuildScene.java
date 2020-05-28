60ead230b4c9958266a15c829ddec00389bd0767
npollock14 <33988166+npollock14@users.noreply.github.com>
2020-05-28 15:59:34 -0700

    cleaning up build scene

--- Drone Constructor - CSQ4 Game/src/BuildScene.java
+++ Drone Constructor - CSQ4 Game/src/BuildScene.java
@@ -4,108 +4,166 @@ import java.awt.Graphics2D;
 
 public class BuildScene extends Scene {
 	Ship s;
-	Part selected = null;
+	Part toBuild = null;
 	int direction = 0;
 	Button selectHull;
 	Button selectArmor;
 	Button selectLaser;
 	Button selectThruster;
-	boolean deleting = false;
+	boolean[] modes = new boolean[4]; // deleting - 0, selection - 1, build - 2, repair - 3
 
 	@Override
 	public void draw(Graphics2D g) {
 		g.fillRect(0, Driver.screenHeight - 150, Driver.screenWidth, 150);
+		g.setColor(Color.white);
+		g.setStroke(new BasicStroke(5));
+		g.drawLine(1600, Driver.screenHeight - 150, 1600, Driver.screenHeight);
+		g.setStroke(new BasicStroke(1));
+		g.setColor(Color.WHITE);
+		g.setFont(Misc.font);
+		g.drawString("Mode:", 1610, 914);
+
 		selectHull.draw(g, 10, 30);
 		selectArmor.draw(g, 10, 30);
 		selectLaser.draw(g, 10, 30);
 		selectThruster.draw(g, 10, 30);
+		g.setColor(Color.black);
 		for (int i = 0; i < Driver.screenWidth; i += 50) {
-			new Point(i, 0).drawDashedLine(g, new Point(i, Driver.screenHeight - 150));
+			new Point(i, 0).drawLine(g, new Point(i, Driver.screenHeight - 150));
 			if (i < Driver.screenHeight - 150)
-				new Point(0, i).drawDashedLine(g, new Point(Driver.screenWidth, i));
+				new Point(0, i).drawLine(g, new Point(Driver.screenWidth, i));
 		}
 		g.setStroke(new BasicStroke((float) (3 * Camera.scale)));
 		Camera.scale = 1.25;
 		for (Part p : s.parts) {
 			p.draw(g, new Point(50 * 18, 50 * 9), 0, new Point(Driver.screenWidth / 2, Driver.screenHeight / 2));
 		}
-		if (!deleting && selected != null && InputManager.mPos.y < Driver.screenHeight - 150) {
-			selected.drawFree(g, new Point((int) ((InputManager.mPos.x / 50)) * Part.SQUARE_WIDTH * 1.25,
-					(int) ((InputManager.mPos.y / 50)) * Part.SQUARE_WIDTH * 1.25));
+		// drawing the free model if in build mode
+		if (modes[2]) {
+			if (InputManager.mPos.y < Driver.screenHeight - 150) {
+				toBuild.drawFree(g, new Point((int) ((InputManager.mPos.x / 50)) * Part.SQUARE_WIDTH * 1.25,
+						(int) ((InputManager.mPos.y / 50)) * Part.SQUARE_WIDTH * 1.25));
+			}
 		}
-		
-		
-		
+
+		// to draw where mouse is - temp
+		if (InputManager.mouse[2]) {
+			System.out.println(InputManager.mPos.toString());
+		}
+		if (modes[0]) {
+			g.setFont(Misc.f);
+			g.setColor(Color.BLACK);
+			g.drawString("Delete Mode", 10, 50);
+		}
+
+	}
+
+	public void changeMode(int m) {
+		for (int i = 0; i < modes.length; i++) {
+			if (m == i) {
+				modes[i] = true;
+			} else
+				modes[i] = false;
+		}
 	}
 
 	@Override
 	public void update() {
+		// update all buttons
 		selectHull.update();
 		selectArmor.update();
 		selectLaser.update();
 		selectThruster.update();
+
+		// check for broken parts
 		s.checkBrokenParts();
-		if (!deleting && selected != null && InputManager.mouseReleased[1]
-				&& InputManager.mPos.y < Driver.screenHeight - 150) {
-			if (selected.type == "Hull") {
-				s.addPart(new Hull(
-						new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9)));
-			}
-			if (selected.type == "Armor") {
-				s.addPart(new Armor(
-						new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9)));
-			}
-			if (selected.type == "Lazer") {
-				s.addPart(new Laser(
-						new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9),
-						direction));
-			}
-			if (selected.type == "Thruster") {
-				s.addPart(new Thruster(
-						new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9),
-						direction));
+
+		if (modes[2]) {
+			if (InputManager.mouseReleased[1] && InputManager.mPos.y < Driver.screenHeight - 150) {
+				if (toBuild.type == "Hull") {
+					s.addPart(new Hull(
+							new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9)));
+				}
+				if (toBuild.type == "Armor") {
+					s.addPart(new Armor(
+							new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9)));
+				}
+				if (toBuild.type == "Lazer") {
+					s.addPart(new Laser(
+							new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9),
+							direction));
+				}
+				if (toBuild.type == "Thruster") {
+					s.addPart(new Thruster(
+							new Point((int) (InputManager.mPos.x / 50) - 18, (int) (InputManager.mPos.y / 50) - 9),
+							direction));
+				}
 			}
 		}
-		if (InputManager.keysReleased[81]) {
-			direction--;
+
+		// handle rotation
+		if (modes[2]) {
+			if (InputManager.keysReleased[81]) {
+				direction--;
+			}
+			if (InputManager.keysReleased[69]) {
+				direction++;
+			}
+
+			direction = Math.abs(direction % 4);
 		}
-		if (InputManager.keysReleased[69]) {
-			direction++;
-		}
+
+		// keybind to switch to delete mode and back to select mode
 		if (InputManager.keysReleased[68]) {
-			deleting ^= true;
-			System.out.println(deleting);
+			if (modes[0]) {
+				changeMode(1);
+			} else {
+				changeMode(0);
+			}
+			System.out.println(modes[0]);
 		}
-		if(InputManager.keysReleased[66]) {
+
+		// handling switching of scenes
+		if (InputManager.keysReleased[66]) {
 			InputManager.keysReleased[66] = false;
 			SceneManager.bs.setActive(false);
 			SceneManager.ms.setActive(true);
 		}
-		direction = Math.abs(direction % 4);
 
-		if (selectHull.clicked)
-			selected = new Hull(new Point(0, 0));
-		if (selectArmor.clicked)
-			selected = new Armor(new Point(0, 0));
-		if (selectLaser.clicked || (selected != null && selected.type.equals("Lazer")
-				&& (InputManager.keysReleased[81] || InputManager.keysReleased[69])))
-			selected = new Laser(new Point(0, 0), direction);
-		if (selectThruster.clicked || (selected != null && selected.type.equals("Thruster")
-				&& (InputManager.keysReleased[81] || InputManager.keysReleased[69])))
-			selected = new Thruster(new Point(0, 0), direction);
+		// handling selection of buttons and rotations
+		if (selectHull.clicked) {
+			toBuild = new Hull(new Point(0, 0));
+			changeMode(2);
+		}
+		if (selectArmor.clicked) {
+			toBuild = new Armor(new Point(0, 0));
+			changeMode(2);
+		}
+		if (selectLaser.clicked || (modes[2] && toBuild.type.equals("Lazer")
+				&& (InputManager.keysReleased[81] || InputManager.keysReleased[69]))) {
+			toBuild = new Laser(new Point(0, 0), direction);
+			changeMode(2);
+		}
+		if (selectThruster.clicked || (modes[2] && toBuild.type.equals("Thruster")
+				&& (InputManager.keysReleased[81] || InputManager.keysReleased[69]))) {
+			toBuild = new Thruster(new Point(0, 0), direction);
+			changeMode(2);
+		}
 
-		if (deleting && InputManager.mouse[1]) {
+		// if deleting
+		if (modes[0] && InputManager.mouse[1]) {
 			for (Part p : s.parts) {
-				if(p.type.equals("Reactor")) continue;
-				Rect r = new Rect((int)(p.pos.x * Part.SQUARE_WIDTH * 1.25 + (50 * 18)),
+				if (p.type.equals("Reactor"))
+					continue;
+				Rect r = new Rect((int) (p.pos.x * Part.SQUARE_WIDTH * 1.25 + (50 * 18)),
 						p.pos.y * Part.SQUARE_WIDTH * 1.25 + (50 * 9), p.width * Part.SQUARE_WIDTH * 1.25,
-						p.height * Part.SQUARE_WIDTH*1.25);
-				if(InputManager.mPos.inside(r, true)) {
+						p.height * Part.SQUARE_WIDTH * 1.25);
+				if (InputManager.mPos.inside(r, true)) {
 					p.health = -1;
 					s.updateCM();
 					break;
 				}
-				
+
 			}
 		}
 
@@ -113,6 +171,9 @@ public class BuildScene extends Scene {
 
 	@Override
 	public void init() {
+
+		changeMode(1);
+
 		selectHull = new Button(new Rect(100, Driver.screenHeight - 100, 50, 50), null, 0, "Hull", null, Color.WHITE,
 				true, false);
 		selectArmor = new Button(new Rect(175, Driver.screenHeight - 100, 50, 50), null, 0, "Armor", null, Color.WHITE,

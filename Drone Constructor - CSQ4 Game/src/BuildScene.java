fc1b76057ed00a1c66f36dc4ce1a293706940e52
npollock14 <33988166+npollock14@users.noreply.github.com>
2020-05-28 22:26:28 -0700

    adding ui stuff to build scene

--- Drone Constructor - CSQ4 Game/src/BuildScene.java
+++ Drone Constructor - CSQ4 Game/src/BuildScene.java
@@ -1,6 +1,7 @@
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
 public class BuildScene extends Scene {
 	Ship s;
@@ -11,50 +12,70 @@ public class BuildScene extends Scene {
 	Button selectLaser;
 	Button selectThruster;
 	boolean[] modes = new boolean[4]; // deleting - 0, selection - 1, build - 2, repair - 3
+	BufferedImage backgroundUI;
+	BufferedImage backgroundUISelection;
 
 	@Override
 	public void draw(Graphics2D g) {
-		g.fillRect(0, Driver.screenHeight - 150, Driver.screenWidth, 150);
-		g.setColor(Color.white);
-		g.setStroke(new BasicStroke(5));
-		g.drawLine(1600, Driver.screenHeight - 150, 1600, Driver.screenHeight);
-		g.setStroke(new BasicStroke(1));
-		g.setColor(Color.WHITE);
-		g.setFont(Misc.font);
-		g.drawString("Mode:", 1610, 914);
+		
 
-		selectHull.draw(g, 10, 30);
-		selectArmor.draw(g, 10, 30);
-		selectLaser.draw(g, 10, 30);
-		selectThruster.draw(g, 10, 30);
+		
+		
+		//draw lines
 		g.setColor(Color.black);
 		for (int i = 0; i < Driver.screenWidth; i += 50) {
-			new Point(i, 0).drawLine(g, new Point(i, Driver.screenHeight - 150));
-			if (i < Driver.screenHeight - 150)
+			new Point(i, 0).drawLine(g, new Point(i, Driver.screenHeight));
+			if (i < Driver.screenHeight)
 				new Point(0, i).drawLine(g, new Point(Driver.screenWidth, i));
 		}
-		g.setStroke(new BasicStroke((float) (3 * Camera.scale)));
+		
+		//draw background on top of lines
+		g.drawImage(backgroundUI, 0, 0, 1920, 1080, null);
+		
+		//draw parts on ship
+		g.setStroke(new BasicStroke((float) (3)));
 		Camera.scale = 1.25;
 		for (Part p : s.parts) {
 			p.draw(g, new Point(50 * 18, 50 * 9), 0, new Point(Driver.screenWidth / 2, Driver.screenHeight / 2));
 		}
+		
+		
 		// drawing the free model if in build mode
 		if (modes[2]) {
-			if (InputManager.mPos.y < Driver.screenHeight - 150) {
+			if (InputManager.mPos.y < 845) {
 				toBuild.drawFree(g, new Point((int) ((InputManager.mPos.x / 50)) * Part.SQUARE_WIDTH * 1.25,
 						(int) ((InputManager.mPos.y / 50)) * Part.SQUARE_WIDTH * 1.25));
 			}
 		}
+		
+		
 
 		// to draw where mouse is - temp
 		if (InputManager.mouse[2]) {
 			System.out.println(InputManager.mPos.toString());
 		}
+		
+		//draw mode - bottom right
+		g.setFont(Misc.f);
+		g.setColor(Color.WHITE);
 		if (modes[0]) {
-			g.setFont(Misc.f);
-			g.setColor(Color.BLACK);
-			g.drawString("Delete Mode", 10, 50);
+			g.drawString("Delete", 1645, 970);
 		}
+		if (modes[1]) {
+			g.drawString("Select", 1645, 970);
+		}
+		if (modes[2]) {
+			g.drawString("Build", 1645, 970);
+		}
+		if (modes[3]) {
+			g.drawString("Repair", 1645, 970);
+		}
+		
+		selectHull.draw(g, 10, 30);
+		selectArmor.draw(g, 10, 30);
+		selectLaser.draw(g, 10, 30);
+		selectThruster.draw(g, 10, 30);
+		
 
 	}
 
@@ -171,10 +192,13 @@ public class BuildScene extends Scene {
 
 	@Override
 	public void init() {
+		
+		backgroundUI = Misc.loadImage("/buildSceneUI.png");
+		backgroundUISelection = Misc.loadImage("/buildSceneUISelection.png");
 
 		changeMode(1);
 
-		selectHull = new Button(new Rect(100, Driver.screenHeight - 100, 50, 50), null, 0, "Hull", null, Color.WHITE,
+		selectHull = new Button(new Rect(331, 889, 80, 80), null, 0, "Hull", null, Color.WHITE,
 				true, false);
 		selectArmor = new Button(new Rect(175, Driver.screenHeight - 100, 50, 50), null, 0, "Armor", null, Color.WHITE,
 				true, false);
